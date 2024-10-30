3feccf46a35315e1a236de1bcfdffeb6d0d35a96
cor1nthian <32943000+cor1nthian@users.noreply.github.com>
2024-10-30 18:59:54 +0300

    Add files via upload

--- cmakebuild.ps1
+++ cmakebuild.ps1
@@ -29,6 +29,15 @@
     Optional. Default - "proj_build". Directory name for created projects. Created projects will reside within this folder,
     each in separate folder per platform/build tool. Script creates thia directory in case its missing
 
+    .PARAMETER CleanupBuildDir
+    Optional. Default - $true. If true, cleanup build directory before starting a build
+
+    .PARAMETER UseFullBuildPaths
+    Optional. Default - $false. If true, use full paths in temporary bat file
+
+    .PARAMETER CheckCmakeLists
+    Optional. Default - $false. If true, check the presence of CmakeLists.txt file
+
     .PARAMETER CMakeListsFName
     Optional. Default - "CMakeLists.txt". CMake lists file name.
 
@@ -95,6 +104,9 @@
 Param ( [Parameter(Position = 0, Mandatory = $true)]   [System.String]   $SourceDir,
         [Parameter(Position = 1, Mandatory = $true)]   [System.String]   $BuildTool,
         [Parameter(Position = 2, Mandatory = $false)]  [System.String]   $BuildDir = "proj_build",
+        [Parameter(Position = 2, Mandatory = $false)]  [System.Boolean]  $CleanupBuildDir = $true,
+        [Parameter(Position = 2, Mandatory = $false)]  [System.Boolean]  $UseFullBuildPaths = $false,
+        [Parameter(Position = 2, Mandatory = $false)]  [System.Boolean]  $CheckCmakeLists = $false,
         [Parameter(Position = 3, Mandatory = $false)]  [System.String]   $CMakeListsFName = "CMakeLists.txt",
         [Parameter(Position = 4, Mandatory = $false)]  [System.String]   $CMakeEXEPath = "$env:PROGRAMFILES\CMake\bin\cmake.exe",
         [Parameter(Position = 5, Mandatory = $false)]  [System.String]   $CMDEXEPath = "$env:SystemRoot\System32\cmd.exe",
@@ -131,170 +143,6 @@ function RemoveFolderContent {
   # Get-ChildItem -Path $FolderPath -Include * -Recurse | ForEach-Object { $_.Delete() }
   Remove-Item "$FolderPath\*" -Recurse -Force
 }
-
-function Remove-FileSystemItem {
-    <#
-    .SYNOPSIS
-      Removes files or directories reliably and synchronously.
-  
-    .DESCRIPTION
-      Removes files and directories, ensuring reliable and synchronous
-      behavior across all supported platforms.
-  
-      The syntax is a subset of what Remove-Item supports; notably,
-      -Include / -Exclude and -Force are NOT supported; -Force is implied.
-      
-      As with Remove-Item, passing -Recurse is required to avoid a prompt when 
-      deleting a non-empty directory.
-  
-      IMPORTANT:
-        * On Unix platforms, this function is merely a wrapper for Remove-Item, 
-          where the latter works reliably and synchronously, but on Windows a 
-          custom implementation must be used to ensure reliable and synchronous 
-          behavior. See https://github.com/PowerShell/PowerShell/issues/8211
-  
-      * On Windows:
-        * The *parent directory* of a directory being removed must be 
-          *writable* for the synchronous custom implementation to work.
-        * The custom implementation is also applied when deleting 
-           directories on *network drives*.
-  
-      * If an indefinitely *locked* file or directory is encountered, removal is aborted.
-        By contrast, files opened with FILE_SHARE_DELETE / 
-        [System.IO.FileShare]::Delete on Windows do NOT prevent removal, 
-        though they do live on under a temporary name in the parent directory 
-        until the last handle to them is closed.
-  
-      * Hidden files and files with the read-only attribute:
-        * These are *quietly removed*; in other words: this function invariably
-          behaves like `Remove-Item -Force`.
-        * Note, however, that in order to target hidden files / directories
-          as *input*, you must specify them as a *literal* path, because they
-          won't be found via a wildcard expression.
-  
-      * The reliable custom implementation on Windows comes at the cost of
-        decreased performance.
-  
-    .EXAMPLE
-      Remove-FileSystemItem C:\tmp -Recurse
-  
-      Synchronously removes directory C:\tmp and all its content.
-    #>
-      [CmdletBinding(SupportsShouldProcess, ConfirmImpact='Medium', DefaultParameterSetName='Path', PositionalBinding=$false)]
-      param(
-        [Parameter(ParameterSetName='Path', Mandatory, Position = 0, ValueFromPipeline, ValueFromPipelineByPropertyName)]
-        [System.String[]] $Path,
-        [Parameter(ParameterSetName='Literalpath', ValueFromPipelineByPropertyName)]
-        [Alias('PSPath')]
-        [System.String[]] $LiteralPath,
-        [System.Management.Automation.SwitchParameter] $Recurse
-      )
-      begin {
-        # !! Workaround for https://github.com/PowerShell/PowerShell/issues/1759
-        if ($ErrorActionPreference -eq [System.Management.Automation.ActionPreference]::Ignore) { $ErrorActionPreference = 'Ignore'}
-        $targetPath = ''
-        $yesToAll = $noToAll = $false
-        function trimTrailingPathSep([string] $itemPath) {
-          if ($itemPath[-1] -in '\', '/') {
-            # Trim the trailing separator, unless the path is a root path such as '/' or 'c:\'
-            if ($itemPath.Length -gt 1 -and $itemPath -notmatch '^[^:\\/]+:.$') {
-              $itemPath = $itemPath.Substring(0, $itemPath.Length - 1)
-            }
-          }
-          $itemPath
-        }
-        function getTempPathOnSameVolume([string] $itemPath, [string] $tempDir) {
-          if (-not $tempDir) { $tempDir = [IO.Path]::GetDirectoryName($itemPath) }
-          [IO.Path]::Combine($tempDir, [IO.Path]::GetRandomFileName())
-        }
-        function syncRemoveFile([string] $filePath, [string] $tempDir) {
-          # Clear the ReadOnly attribute, if present.
-          if (($attribs = [IO.File]::GetAttributes($filePath)) -band [System.IO.FileAttributes]::ReadOnly) {
-            [IO.File]::SetAttributes($filePath, $attribs -band -bnot [System.IO.FileAttributes]::ReadOnly)
-          }
-          $tempPath = getTempPathOnSameVolume $filePath $tempDir
-          [IO.File]::Move($filePath, $tempPath)
-          [IO.File]::Delete($tempPath)
-        }
-        function syncRemoveDir([string] $dirPath, [switch] $recursing) {
-            if (-not $recursing) { $dirPathParent = [IO.Path]::GetDirectoryName($dirPath) }
-            # Clear the ReadOnly attribute, if present.
-            # Note: [IO.File]::*Attributes() is also used for *directories*; [IO.Directory] doesn't have attribute-related methods.
-            if (($attribs = [IO.File]::GetAttributes($dirPath)) -band [System.IO.FileAttributes]::ReadOnly) {
-              [IO.File]::SetAttributes($dirPath, $attribs -band -bnot [System.IO.FileAttributes]::ReadOnly)
-            }
-            # Remove all children synchronously.
-            $isFirstChild = $true
-            foreach ($item in [IO.directory]::EnumerateFileSystemEntries($dirPath)) {
-              if (-not $recursing -and -not $Recurse -and $isFirstChild) { # If -Recurse wasn't specified, prompt for nonempty dirs.
-                $isFirstChild = $false
-                # Note: If -Confirm was also passed, this prompt is displayed *in addition*, after the standard $PSCmdlet.ShouldProcess() prompt.
-                #       While Remove-Item also prompts twice in this scenario, it shows the has-children prompt *first*.
-                if (-not $PSCmdlet.ShouldContinue("The item at '$dirPath' has children and the -Recurse switch was not specified. If you continue, all children will be removed with the item. Are you sure you want to continue?", 'Confirm', ([ref] $yesToAll), ([ref] $noToAll))) { return }
-              }
-              $itemPath = [IO.Path]::Combine($dirPath, $item)
-              ([ref] $targetPath).Value = $itemPath
-              if ([IO.Directory]::Exists($itemPath)) {
-                syncremoveDir $itemPath -recursing
-              } else {
-                syncremoveFile $itemPath $dirPathParent
-              }
-            }
-            # Finally, remove the directory itself synchronously.
-            ([ref] $targetPath).Value = $dirPath
-            $tempPath = getTempPathOnSameVolume $dirPath $dirPathParent
-            [IO.Directory]::Move($dirPath, $tempPath)
-            [IO.Directory]::Delete($tempPath)
-        }
-      }
-  
-      process {
-        $isLiteral = $PSCmdlet.ParameterSetName -eq 'LiteralPath'
-        if ($env:OS -ne 'Windows_NT') { # Unix: simply pass through to Remove-Item, which on Unix works reliably and synchronously
-          Remove-Item @PSBoundParameters
-        } else { # Windows: use synchronous custom implementation
-          foreach ($rawPath in ($Path, $LiteralPath)[$isLiteral]) {
-            # Resolve the paths to full, filesystem-native paths.
-            try {
-              # !! Convert-Path does find hidden items via *literal* paths, but not via *wildcards* - and it has no -Force switch (yet)
-              # !! See https://github.com/PowerShell/PowerShell/issues/6501
-              $resolvedPaths = if ($isLiteral) { Convert-Path -ErrorAction Stop -LiteralPath $rawPath } else { Convert-Path -ErrorAction Stop -path $rawPath}
-            } catch {
-              Write-Error $_ # relay error, but in the name of this function
-              continue
-            }
-            try {
-              $isDir = $false
-              foreach ($resolvedPath in $resolvedPaths) {
-                # -WhatIf and -Confirm support.
-                if (-not $PSCmdlet.ShouldProcess($resolvedPath)) { continue }
-                if ($isDir = [IO.Directory]::Exists($resolvedPath)) { # dir.
-                  # !! A trailing '\' or '/' causes directory removal to fail ("in use"), so we trim it first.
-                  syncRemoveDir (trimTrailingPathSep $resolvedPath)
-                } elseif ([IO.File]::Exists($resolvedPath)) { # file
-                  syncRemoveFile $resolvedPath
-                } else {
-                  Throw "Not a file-system path or no longer extant: $resolvedPath"
-                }
-              }
-            } catch {
-              if ($isDir) {
-                $exc = $_.Exception
-                if ($exc.InnerException) { $exc = $exc.InnerException }
-                if ($targetPath -eq $resolvedPath) {
-                  Write-Error "Removal of directory '$resolvedPath' failed: $exc"
-                } else {
-                  Write-Error "Removal of directory '$resolvedPath' failed, because its content could not be (fully) removed: $targetPath`: $exc"
-                }
-              } else {
-                Write-Error $_  # relay error, but in the name of this function
-              }
-              continue
-            }
-          }
-        }
-      }
-  }
 
 function GetPSVersion {
     
@@ -430,8 +278,8 @@ function BAT2File {
 function CreateBAT {
 
     Param ( [Parameter(Position = 0, Mandatory = $true)]  [System.Version] $CMakeVersion,
-            [Parameter(Position = 1, Mandatory = $false)] [System.Boolean] $FullBuildPaths = $true )
-    
+            [Parameter(Position = 1, Mandatory = $false)] [System.Boolean] $FullBuildPaths = $script:UseFullBuildPaths )
+
     [System.String] $line
     [System.String[]] $Cmds = @( "cd `"$script:FULLBUILDPATH`"" )
     if($CMakeVersion -ge [System.Version] $script:BridgeCMakeVersion) {
@@ -509,14 +357,14 @@ function EndScript {
     if(($null -ne $Message) -and ($Message.Length -gt 0)) {
         WriteColored $Message
     }
-    [System.Void][System.Console]::ReadKey($true)
-    if(($null -ne $Message) -or ($Message.Length -gt 0)) {
-        WriteColored "`nBUILD ERROR OCCURED; RESS ANY KEY TO CONTINUE" $script:MSGERRORCOLOR
+    if(($null -ne $Message) -and ($Message.Length -gt 0)) {
+        WriteColored "`nBUILD ERROR OCCURED; PRESS ANY KEY TO CONTINUE" $script:MSGERRORCOLOR
         [Environment]::Exit(1)
     } else {
         WriteColored "`nPRESS ANY KEY TO CONTINUE" $script:MSGCOLOR
         [Environment]::Exit(0)
     }
+    [System.Void][System.Console]::ReadKey($true)
 }
 
 ######################################
@@ -528,8 +376,10 @@ if((GetPSVersion $true) -lt $PSVERSIONMIN) {
 if(!(FSObjectExists $SourceDir) -or !(IsDir $SourceDir)) {
     EndScript "INCORRECT BUILD FOLDER SPECIFIED"
 }
-if(!(FSObjectExists "$SourceDir\$CMakeListsFName") -or !(IsFile "$SourceDir\$CMakeListsFName")) {
-    EndScript "CMAKELISTS.TXT FILE NOT FOUND IN SOURCE DIR"
+if($CheckCmakeLists) {
+    if(!(FSObjectExists "$SourceDir\$CMakeListsFName") -or !(IsFile "$SourceDir\$CMakeListsFName")) {
+        EndScript "CMAKELISTS.TXT FILE NOT FOUND IN SOURCE DIR"
+    }
 }
 if(!(CreateFolder $FULLBUILDPATH)) {
     EndScript "FAILED TO CREATE BUILD FOLDER"
@@ -556,9 +406,11 @@ if($null -eq $BPath) {
     EndScript "COULDNT CREATE BAT FILE"
 }
 $BPath = $BPath.Trim()
-RemoveFolderContent $FULLBUILDPATH
+if($CleanupBuildDir ) {
+    RemoveFolderContent $FULLBUILDPATH
+}
 & $CMDEXEPath "/c `"$BPath`""
-Remove-FileSystemItem $BPath
+Remove-Item $BPath -Force
 EndScript
 
 # Add-Type -AssemblyName System.Windows.Forms
